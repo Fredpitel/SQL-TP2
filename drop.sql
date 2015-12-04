db7b96a2b3181852c2578eeb9839fe5061cbaffb
Fredpitel <fredpitel@gmail.com>
2015-12-04 16:06:29 -0500

    update avec triggers

--- drop.sql
+++ drop.sql
@@ -5,22 +5,39 @@
 -- Code permanent: BLAK29019305
 -- 
 SET ECHO ON
-DROP TABLE Mesure
+
+DROP TABLE Mesure;
 /
-DROP TABLE Individu
+DROP TABLE Individu;
 /
-DROP TABLE Espece
+DROP TABLE Espece;
 /
-DROP TABLE Choix
+DROP TABLE Choix;
 /
-DROP TABLE Surveillance
+DROP TABLE Surveillance;
 /
-DROP TABLE Lotissement
+DROP TABLE Lotissement;
 /
-DROP TABLE Zones
+DROP TABLE Zones;
 /
-DROP TABLE Salaire
+DROP TABLE Salaire;
 /
-DROP TABLE Employe
+DROP TABLE Employe;
+/
+DROP TRIGGER DateCoherente;
+/
+DROP TRIGGER FonctionCoherente;
+/
+DROP TRIGGER TauxEtGradeNonNul;
+/
+DROP TRIGGER UneSeuleZoneParJour;
+/
+DROP TRIGGER LotissementDifferentParHeure;
+/
+DROP TRIGGER UnSeulSurveillant;
+/
+DROP TRIGGER LotissementsMemeZone;
+/
+DROP TRIGGER DateMesureDifferente;
 /
 SET ECHO OFF
