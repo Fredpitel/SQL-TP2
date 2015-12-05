7f7ebea4a85d211aa466b0a5fb734dd5b6003a38
Fredpitel <fredpitel@gmail.com>
2015-12-05 13:05:11 -0500

    Ajouté Rollback

--- tests.sql
+++ tests.sql
@@ -14,29 +14,42 @@ SPOOL tests.res;
 
 INSERT INTO Employe VALUES ('AAA', '012345678', 'Date', 'Mauvaise', NULL, '2000-10-03', '2020 du finfin', '5142222222', 'Secrétaire', 'Administratif', NULL, NULL)
 /
-
+ROLLBACK
+/
 -- Tests FonctionCoherente
 
 INSERT INTO Employe VALUES ('BBB', '123456789', 'Fonction', 'Mauvaise', NULL, '1985-10-03', '2020 du finfin', '514-222-2222', 'Surveillant', 'Administratif', '50', 'G1')
 /
+ROLLBACK
+/
 
 INSERT INTO Employe VALUES ('CCC', '234567890', 'Fonction', 'Mauvaise', NULL, '1985-10-03', '2020 du finfin', '514-222-2222', 'Surveillant', 'Médical', '50', 'G1')
 /
+ROLLBACK
+/
 
 INSERT INTO Employe VALUES ('DDD', '345678901', 'Fonction', 'Mauvaise', NULL, '1985-10-03', '2020 du finfin', '514-222-2222', 'Secrétaire', 'Surveillance', NULL, NULL)
+/
+ROLLBACK
 /
 
 -- Tests TauxEtGradeNonNul
 
 INSERT INTO Employe VALUES ('EEE', '456789012', 'Grade', 'Mauvais', NULL, '1985-10-03', '2020 du finfin', '514-222-2222', 'Surveillant', 'Surveillance', 50, NULL)
 /
+ROLLBACK
+/
 
 INSERT INTO Employe VALUES ('FFF', '567890123', 'Taux', 'Mauvais', NULL, '1985-10-03', '2020 du finfin', '514-222-2222', 'Surveillant', 'Surveillance', NULL, 'G1')
+/
+ROLLBACK
 /
 
 -- Tests UneSeuleZoneParJour
 
 INSERT INTO Surveillance VALUES ('IJH', 2, 1, 'Lundi', 12)
+/
+ROLLBACK
 /
 
 -- Tests LotissementParHeurePrecedent
@@ -44,11 +57,15 @@ INSERT INTO Surveillance VALUES ('IJH', 2, 1, 'Lundi', 12)
 INSERT INTO Surveillance
  	VALUES('IJH',1,	2,'Lundi',11)
 /
+ROLLBACK
+/
 
 -- Tests LotissementParHeureSuivant
 
 INSERT INTO Surveillance
  	VALUES('IJH',2,	2,'Mardi',15)
+/
+ROLLBACK
 /
 
 -- Tests UnSeulSurveillant
@@ -56,11 +73,15 @@ INSERT INTO Surveillance
 INSERT INTO Surveillance
  	VALUES('JHK',1,	1,'Lundi',9)
 /
+ROLLBACK
+/
 
 -- Tests HeureDifferente
 
 INSERT INTO Surveillance
  	VALUES('IJH',1,	2,'Lundi',9)
+/
+ROLLBACK
 /
 
 SPOOL OFF;
