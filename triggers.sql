4c1731c18b14727f66183972f099c8b7f64e84c2
Fredpitel <fredpitel@gmail.com>
2015-12-06 13:46:28 -0500

    Final

--- triggers.sql
+++ triggers.sql
@@ -237,7 +237,7 @@ SELECT CodeEspece INTO especeMere
 FROM Individu
 WHERE CodeIndividu = :ligneApres.Mere;
 IF(especePere = especeMere AND especePere <> :ligneApres.CodeEspece AND NOT especePere IS NULL)
-THEN RAISE_APPLICATION_ERROR(-20014,'Si les parent sont de la meme espèce alors l''enfant doit être lui aussi de la même espèce');
+THEN RAISE_APPLICATION_ERROR(-20014,'Si les parent sont de la même espèce alors l''enfant doit être lui aussi de la même espèce');
 END IF;
 EXCEPTION WHEN NO_DATA_FOUND THEN NULL;
 END;
